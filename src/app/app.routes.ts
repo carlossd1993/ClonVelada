b7487816d32451594fa2f19ccac601bd51c06ee3
carlossd1993 <161085364+carlossd1993@users.noreply.github.com>
2024-05-15 18:01:00 +0200

    Añadida página pronosticos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {CombatesComponent} from "./pages/combates/combates.component";
 import {CombatDetailComponent} from "./pages/combat-detail/combat-detail.component";
+import {PronosticosComponent} from "./pages/pronosticos/pronosticos.component";
 
 export const routes: Routes = [
   {path: "combates", component: CombatesComponent},
   {path: "combate/:id", component: CombatDetailComponent},
+  {path: "pronosticos", component: PronosticosComponent},
   {path: "**", component: HomeComponent}
 ];

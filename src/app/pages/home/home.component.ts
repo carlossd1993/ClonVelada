1431bb7e6444cdfe387805e762a45657bc6c8558
carlossd1993 <161085364+carlossd1993@users.noreply.github.com>
2024-05-15 17:18:51 +0200

    Añadido video presentación al home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import {HeroComponent} from "../../components/hero/hero.component";
 import {ContadorComponent} from "../../components/contador/contador.component";
 import {BoxerSelectionComponent} from "../../components/boxer-selection/boxer-selection.component";
 import {SeleccionBoxeadoresComponent} from "../../components/seleccion-boxeadores/seleccion-boxeadores.component";
+import {VideoComponent} from "../../components/video/video.component";
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,8 @@ import {SeleccionBoxeadoresComponent} from "../../components/seleccion-boxeadore
     HeroComponent,
     ContadorComponent,
     BoxerSelectionComponent,
-    SeleccionBoxeadoresComponent
+    SeleccionBoxeadoresComponent,
+    VideoComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
